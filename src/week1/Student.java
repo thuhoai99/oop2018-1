4dcdaa4d7ef734e2b20688fb39a797d93a4ff27a
maihoai99 <43167086+maihoai99@users.noreply.github.com>
2018-09-16 11:45:39 +0700

    Bai tap

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,16 +3,38 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private String group;
+    private String id;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String n) {
+        this.name = n;
+    }
+    public void setid(String n) {
+        this.id = n;
+    }
+    public void setGroup(String n) {
+        this.group = n;
+    }
+    public void setEmail(String n) {
+        this.email = n;
+    }
+
 
     /**
      * Constructor 1
      */
-    Student() {
-        // TODO:
+    Student()
+    {
+        name = "Student";
+        id = "000";
+        group = "INT22041";
+        email = "uet@vnu.edu.vn";
     }
-
     /**
      * Constructor 2
      * @param n
@@ -21,6 +43,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name = n;
+        id = sid;
+        email = em;
+        group = "INT22041";
     }
 
     /**
@@ -28,11 +54,17 @@ public class Student {
      * @param s
      */
     Student(Student s) {
+        name = s.name;
+        group = s.group;
+        id = s.id;
+        email = s.email;
         // TODO:
     }
 
     String getInfo() {
-        // TODO:
+
+        System.out.println("Name: " + this.name + "\nId: "+ this.id +"\nGroup: "
+                    +this.group + "\nEmail: "+this.email);
         return null; // xóa dòng này sau khi cài đặt
     }
 }
